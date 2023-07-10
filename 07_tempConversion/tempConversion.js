ebf288bc7b389cee8eb868d7d123976cb8e34c5f
Deo Seth <diothewind@gmail.com>
2023-07-10 18:49:46 +0800

    Finish seventh JS exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  const celsius = ((fah - 32) * 5) / 9;
+  return +celsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  const fahrenheit = (cel * 1.8) + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
