c24174edb75c55c51d7267cb3059a33d5f6d41c3
Deo Seth <diothewind@gmail.com>
2023-07-10 14:40:09 +0800

    Finish third JS exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    revS = [];
+    for (let i = string.length - 1; i >= 0; i--) {
+        revS.push(string.charAt(i));
+    }   
 
+    return revS.join("");
 };
 
 // Do not edit below this line
