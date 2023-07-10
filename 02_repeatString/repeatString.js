a2285ef07e7e4d2184b40eb3cf30565cb3db8778
Deo Seth <diothewind@gmail.com>
2023-07-10 14:31:34 +0800

    Finish second JS exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(s, num) {
+    let repeatString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 1; i <= num; i++) {
+        repeatString += s;
+    }
 
+    return repeatString;
 };
 
 // Do not edit below this line
