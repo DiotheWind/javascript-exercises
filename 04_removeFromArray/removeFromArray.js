dc10400eef7826001595484c7e73c491b11f0fef
Deo Seth <diothewind@gmail.com>
2023-07-10 16:52:22 +0800

    Finish fourth JS exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, toDelete) {
+    const toRemove = Array.from(arguments)
+    toRemove.shift();
+    const newArr = [];
 
+    outer:
+    for (const elements of arr) {
+        for (let i = 0; i <= toRemove.length - 1; i++) {
+            if (elements === toRemove[i]) {
+                continue outer;
+            }
+        }
+
+        newArr.push(elements)
+    }
+    return newArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
