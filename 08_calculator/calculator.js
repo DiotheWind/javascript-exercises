fdd3be5eae9e1d93d7c2c438ae15a1c0127823ce
Deo Seth <diothewind@gmail.com>
2023-07-22 21:09:16 +0800

    Finish 8th JS exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((prev, currentN) => prev + currentN, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...num) {
+  return num.reduce((prev, currentN) => prev * currentN);
 };
 
-const power = function() {
-	
+const power = function(n,p) {
+	return n**p;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0) {
+    return 1;
+  } else return n * factorial(n-1);
 };
 
 // Do not edit below this line
