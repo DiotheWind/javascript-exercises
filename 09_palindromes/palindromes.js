66dc9c06880ca5dba962d458c1fba90851f10469
Deo Seth <diothewind@gmail.com>
2023-07-23 20:50:24 +0800

    Finish 9th JS exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/\W/g, "").split("");
+    const strReverse = str.slice().reverse();
+    return (str.join("").toLowerCase() === strReverse.join("").toLowerCase());
 };
 
 // Do not edit below this line
