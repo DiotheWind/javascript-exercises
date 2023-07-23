4470eb5ebdd8de7bc0a4ec74f237d27590a76370
Deo Seth <diothewind@gmail.com>
2023-07-23 21:22:46 +0800

    Finish 10th js exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n === 1 || n === 2) return 1;
 
+    let a = 1;
+    let b = 1;
+    let c;
+    for (let i = 3; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
 };
 
 // Do not edit below this line
