03244ea3f62220bde5aeba99c921349c05ce3f59
Deo Seth <diothewind@gmail.com>
2023-07-24 16:54:00 +0800

    Finish 12th JS exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sortedArr = arr.sort((a,b) => {
+        if (!("yearOfDeath" in a)) {
+            a["yearOfDeath"] = new Date().getFullYear();
+        };
+        if (!("yearOfDeath" in b)) {
+            b["yearOfDeath"] = new Date().getFullYear();
+        };
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge - aAge;
+    });
+    return sortedArr[0];
 };
 
 // Do not edit below this line
